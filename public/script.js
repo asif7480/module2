82d4df01e09d7fd5d2619f4e6c084d9237a59368
asif7480 <a.a.khan7480@gmail.com>
2022-08-14 01:20:15 +0500

    Pushing after creating and deploying on to firebase

---
+++ public/script.js
@@ -0,0 +1,48 @@
+const validateUserName = () => {
+  let text = document.querySelector("#text").value;
+  if (text[0] === text[0].toUpperCase()) {
+    return true;
+  } else {
+    alert("Enter First letter capital of full name.");
+    return false;
+  }
+};
+
+const validatePassword = () => {
+  let password = document.querySelector("#password").value;
+  if (
+    password.length >= 8 &&
+    password.match(/[0-9]/g) &&
+    password.match(/[a-z]/g) &&
+    password.match(/[A-Z]/g) &&
+    password.match(/[^a-zA-Z\d]/g)
+  ) {
+    console.log("validatePassword Function called");
+    return true;
+  } else {
+    alert(
+      "Password Should Contain Atleast 1 Number, 1 Upppercase Letter, 1 Lowercase Letter, 1 Special Character and Length of 8 Characters or more."
+    );
+    return false;
+  }
+};
+
+const confirmPassword = () => {
+  let password = document.querySelector("#password");
+  let repeatPassword = document.querySelector("#repeatPassword");
+
+  if (password.value == repeatPassword.value) {
+    return true;
+  } else {
+    alert("Confirm password should be equal to password");
+    return false;
+  }
+};
+
+const signUpValidation = () => {
+  if(validateUserName() && validatePassword() && confirmPassword() ){
+    return true;
+  }else{
+    return false;
+  }
+}
